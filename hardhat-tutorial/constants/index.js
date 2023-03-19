a113532f7952541fb6142625bfeb0009181b1bc1
Shiva-Sai-ssb <shivas.bojjalwar@gmail.com>
2023-03-19 12:46:57 +0530

    Complete

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xeeF93A395e11F778C8A6856BEda9D33F02669b7F";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
